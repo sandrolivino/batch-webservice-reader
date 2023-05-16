07e88a52ac610c9f341b312d79e80c40365a88dc
Sandro Livino <sandro.livino@gmail.com>
2023-05-16 09:08:19 -0300

    Fetch dos usuários a partir da API - Criação do método List<User> fetchUserDataFromAPI()

--- src/main/java/com/example/servicereaderjob/reader/UserReader.java
+++ src/main/java/com/example/servicereaderjob/reader/UserReader.java
@@ -1,19 +1,36 @@
 package com.example.servicereaderjob.reader;
 
+import com.example.servicereaderjob.domain.ResponseUser;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.example.servicereaderjob.domain.User;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Component
 public class UserReader implements ItemReader<User> {
-  
+    private RestTemplate restTemplate = new RestTemplate();
 
-  @Override
-  public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-    //TODO: Implementar leitura
-  }
+    private int page = 1;
+
+    @Override
+    public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+
+    }
+
+    private List<User> fetchUserDataFromAPI() {
+      ResponseEntity<ResponseUser> response = restTemplate.getForEntity(
+              String.format("https://gorest.co.in/public/v1/users?page=%d", page),
+              ResponseUser.class);
+
+      List<User> userData = response.getBody().getData();
+
+      return userData;
+    }
 }
